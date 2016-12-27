55465c49d5d9be044bdab9e81aea50c22feabe6d
Shane Holloway <shane.holloway@ieee.org>
2016-12-27 15:56:20 -0700

    Updated tests to use offside-js syntax as well

--- test/_xform_example.js
+++ test/_xform_example.js
@@ -3,36 +3,39 @@ const fs = require('fs')
 const path = require('path')
 const babel = require('babel-core')
 
-const babel_opt = {
-  plugins: [path.resolve(__dirname, '../dist/')],
-  //sourceMaps: 'inline',
-}
+const babel_opt = 
+  @{} plugins: [path.resolve(__dirname, '../dist/')]
+    , sourceMaps: 'inline'
 
-Object.assign(exports, {
-  babel_opt,
-  transformExampleCode,
-  showFormattedOutput,
-})
+Object.assign @ exports, 
+  @{} babel_opt
+    , transformExampleCode
+    , showFormattedOutput
 
-function transformExampleCode(filename, show=null) {
+function transformExampleCode(filename, show=null) ::
   filename = path.resolve(__dirname, filename)
   if (show && 'function' !== typeof show)
     show = showFormattedOutput
 
-  return new Promise((resolve, reject) =>
-    fs.readFile(filename, 'utf-8', (err, original) => {
-      if (err) return reject(err)
+  return new Promise
+    @ (resolve, reject) => ::()
+      fs.readFile @ filename, 'utf-8', (err, original) => ::
+        if (err) return reject(err)
 
-      try {
-        let res = babel.transform(original, babel_opt)
-        if (show) show('Original', original)
-        if (show) show('Transformed', res.code)
-        resolve(res.code)
-      } catch (err) { reject(err) }
-    }) ).catch(err => (console.error(err), Promise.reject(err)))
-}
+        try ::
+          let res = babel.transform(original, babel_opt)
+          if (show) show('Original', original)
+          if (show) show('Transformed', res.code)
+          resolve(res.code)
 
-function showFormattedOutput(label, code) {
+        catch (err) ::
+          reject(err)
+      
+    .catch @ err => ::@
+      console.error(err), Promise.reject(err)
+
+
+function showFormattedOutput(label, code) ::
   console.log()
   console.log(`#### ${label}:`)
   console.log()
@@ -40,5 +43,4 @@ function showFormattedOutput(label, code) {
   console.log(code)
   console.log('```')
   console.log()
-}
 
