ef1ee1f70d977fc2ad0aa2cf8fc41ce28a2b7f24
Shane Holloway <shane.holloway@ieee.org>
2017-02-23 00:26:38 -0700

    Added tests for switch statements

---
+++ test/testSwitchStatements.js
@@ -0,0 +1,43 @@
+require('source-map-support').install()
+const {genSyntaxTestCases, standardTransforms} = require('./_xform_syntax_variations')
+
+const tap = require('tap-lite-tester')
+tap.start()
+
+genSyntaxTestCases @ tap, iterSyntaxVariations()
+if 1 ::
+  for let xform of Object.values @ standardTransforms ::
+    genSyntaxTestCases @ tap, xform @ iterSyntaxVariations()
+
+tap.finish()
+
+
+function * iterSyntaxVariations() ::
+  // switch (expr) :: cases variations
+  yield :: expectValid: true
+    , title: 'vanilla switch statement'
+    , source: @[] 'switch (expr) { case a: default: break }'
+    , tokens: @[] 'switch', '(', 'name', ')', '{', 'case', 'name', ':', 'default', ':', 'break', '}', 'eof'
+
+  yield :: expectValid: true
+    , title: 'offside switch statement'
+    , source: @[] 'switch (expr) :: case a: default: break'
+    , tokens: @[] 'switch', '(', 'name', ')', '{', 'case', 'name', ':', 'default', ':', 'break', '}', 'eof'
+
+  yield :: expectValid: true
+    , title: 'keyword offside switch statement'
+    , source: @[] 'switch expr :: case a: default: break'
+    , tokens: @[] 'switch', '(', 'name', ')', '{', 'case', 'name', ':', 'default', ':', 'break', '}', 'eof'
+
+  yield :: expectValid: true
+    , title: 'keyword @ offside switch statement'
+    , source: @[] 'switch @ expr :: case a: default: break'
+    , tokens: @[] 'switch', '(', '(', 'name', ')', ')', '{', 'case', 'name', ':', 'default', ':', 'break', '}', 'eof'
+
+  yield :: expectValid: true
+    , title: 'keyword offside switch statement with call'
+    , source: @[] 'switch fn_call @ x, y :: case a: default: break'
+    , tokens: @[] 'switch', '(', 'name', '(', 'name', ',', 'name', ')', ')', '{', 'case', 'name', ':', 'default', ':', 'break', '}', 'eof'
+
+   
+
--- test/testWhileStatements.js
+++ test/testWhileStatements.js
@@ -5,7 +5,7 @@ const tap = require('tap-lite-tester')
 tap.start()
 
 genSyntaxTestCases @ tap, iterSyntaxVariations()
-if 0 ::
+if 1 ::
   for let xform of Object.values @ standardTransforms ::
     genSyntaxTestCases @ tap, xform @ iterSyntaxVariations()
 
