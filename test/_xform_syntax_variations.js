cad7a728e76b87c898dd4df576bb07da2ed06583
Shane Holloway <shane.holloway@ieee.org>
2017-03-08 19:00:22 -0700

    Added tests for async generator syntax; expanded token based testing with pre and post transform options

--- test/_xform_syntax_variations.js
+++ test/_xform_syntax_variations.js
@@ -42,9 +42,13 @@ function testSourceTransform(t, testCase) ::
   if (testCase.tokens) ::
     const tokens = res.ast.tokens
       .map @ token => token.type.label
+    t.deepEqual @ tokens.pop(), 'eof'
+
     if ('tokens' === testCase.debug) ::
       console.log @ tokens
-    t.deepEqual @ tokens, testCase.tokens
+    const expected_tokens = Array.from(testCase.tokens)
+      .filter @ token => token !== 'eof'
+    t.deepEqual @ tokens, expected_tokens
 
 
 function genSyntaxTestCases(tap, iterable_test_cases) ::
@@ -63,10 +67,19 @@ function genSyntaxTestCases(tap, iterable_test_cases) ::
     else ::
       tap.test @ title, testFn
 
-function bindIterableTransform(title_suffix, prefix, postfix, indent=2) ::
-  indent = ' '.repeat @ indent
+function bindIterableTransform(title_suffix, prefix, postfix, options={}) ::
+  if 'string' !== typeof prefix ::
+    throw new Error("Expected prefix to be a string")
+  if postfix && 'string' !== typeof postfix ::
+    options = postfix; postfix = null
+
+  const indent = ' '.repeat @ options.indent || 2
+
+  let pre_tokens = options.pre_tokens, post_tokens = options.post_tokens
+  if !pre_tokens && options.tokens :: pre_tokens = tokens
+
   return function * (iterable_test_cases) ::
-    for (let testCase of iterable_test_cases) ::
+    for (const testCase of iterable_test_cases) ::
       const title = `${testCase.title} WITHIN ${title_suffix}`
       const source = [].concat @
           [prefix || '']
@@ -74,34 +87,86 @@ function bindIterableTransform(title_suffix, prefix, postfix, indent=2) ::
         , ['']
         , [postfix || '']
 
-      yield Object.assign @ {}, testCase, @{} title, source, tokens: null
+      let tokens = null
+      if testCase.tokens && (pre_tokens || post_tokens) ::
+        tokens = [].concat @ pre_tokens || [], testCase.tokens || [], post_tokens || []
 
-const standardTransforms = ::
-    inBlock: bindIterableTransform @ 'vanilla block', '{', '}'
-  , inOffsideBlock: bindIterableTransform @ 'offside block', '::'
-  , inFunction: bindIterableTransform @ 'vanilla function', 'function outer_fn() {', '}'
-  , inOffsideFn: bindIterableTransform @ 'offside function', 'function outer() ::'
-  , inArrowFn: bindIterableTransform @ 'vanilla arrow function', 'const outer_arrow = () => {', '}'
-  , inOffsideArrowFn: bindIterableTransform @ 'offside arrow function', 'const outer_arrow = () => ::'
-  , inIfBlock: bindIterableTransform @ 'keyword offside if block', 'if expr_0 ::'
-  , inWhileBlock: bindIterableTransform @ 'keyword offside while block', 'while expr_0 ::'
-  , inSwitchBlock: bindIterableTransform @ 'keyword offside switch block', 'switch expr_0 ::\n  case a: default:', '', 4
+      yield Object.assign @ {}, testCase, @{} title, source, tokens
 
-  , inFinallyBlock: bindIterableTransform @ 'offside finally block', 'try ::\nfinally ::'
-  , inTryFinallyBlock: bindIterableTransform @ 'offside try/finally block', 'try ::', 'finally ::'
-  , inCatchBlock: bindIterableTransform @ 'keyword offside try/finally block', 'try ::\ncatch err ::'
-  , inTryCatchBlock: bindIterableTransform @ 'offside try/catch block', 'try ::', 'catch (err) ::'
 
-  // TODO: Investigate while the following causes errors
+const blockTransforms = ::
+    inBlock: bindIterableTransform @ 'vanilla block', '{', '}',
+      @{} pre_tokens: @[] '{'
+        , post_tokens: @[] '}'
+  , inOffsideBlock: bindIterableTransform @ 'offside block', '::',
+      @{} pre_tokens: @[] '{'
+        , post_tokens: @[] '}'
+  , inIfBlock: bindIterableTransform @ 'keyword offside if block', 'if expr_0 ::',
+      @{} pre_tokens: @[] 'if', '(', 'name', ')', '{'
+        , post_tokens: @[] '}'
+  , inWhileBlock: bindIterableTransform @ 'keyword offside while block', 'while expr_0 ::',
+      @{} pre_tokens: @[] 'while', '(', 'name', ')', '{'
+        , post_tokens: @[] '}'
+  , inSwitchBlock: bindIterableTransform @ 'keyword offside switch block', 'switch expr_0 ::\n  case a: default:',
+      @{} indent: 4
+        , pre_tokens: @[] 'switch', '(', 'name', ')', '{', 'case', 'name', ':', 'default', ':'
+        , post_tokens: @[] '}'
+  , inFinallyBlock: bindIterableTransform @ 'offside finally block', 'try ::\nfinally ::',
+      @{} pre_tokens: @[] 'try', '{', '}', 'finally', '{'
+        , post_tokens: @[] '}'
+  , inTryFinallyBlock: bindIterableTransform @ 'offside try/finally block', 'try ::', 'finally ::',
+      @{} pre_tokens: @[] 'try', '{'
+        , post_tokens: @[] '}', 'finally', '{', '}'
+  , inCatchBlock: bindIterableTransform @ 'keyword offside try/finally block', 'try ::\ncatch err ::',
+      @{} pre_tokens: @[] 'try', '{', '}', 'catch', '(', 'name', ')', '{'
+        , post_tokens: @[] '}'
+  , inTryCatchBlock: bindIterableTransform @ 'offside try/catch block', 'try ::', 'catch (err) ::',
+      @{} pre_tokens: @[] 'try', '{'
+        , post_tokens: @[] '}', 'catch', '(', 'name', ')', '{', '}'
+
+  // TODO: Investigate why the following causes errors
   //, inTryCatchBlock_v2: bindIterableTransform @ 'keyword offside try/catch block', 'try ::', 'catch err ::'
 
-const extendedTransforms = ::
+const functionTransforms = ::
+    inFunction: bindIterableTransform @ 'vanilla function', 'function outer_fn() {', '}',
+      @{} pre_tokens: @[] 'function', 'name', '(', ')', '{'
+        , post_tokens: @[] '}'
+  , inOffsideFn: bindIterableTransform @ 'offside function', 'function outer_fn() ::',
+      @{} pre_tokens: @[] 'function', 'name', '(', ')', '{'
+        , post_tokens: @[] '}'
+  , inArrowFn: bindIterableTransform @ 'vanilla arrow function', 'const outer_arrow = () => {', '}',
+      @{} pre_tokens: @[] 'const', 'name', '=', '(', ')', '=>', '{'
+        , post_tokens: @[] '}'
+  , inOffsideArrowFn: bindIterableTransform @ 'offside arrow function', 'const outer_arrow = () => ::',
+      @{} pre_tokens: @[] 'const', 'name', '=', '(', ')', '=>', '{'
+        , post_tokens: @[] '}'
+
+const asyncFunctionTransforms = ::
+    inAsyncFunction: bindIterableTransform @ 'vanilla async function', 'async function outer_fn() {', '}',
+      @{} pre_tokens: @[] 'name', 'function', 'name', '(', ')', '{'
+        , post_tokens: @[] '}'
+  , inOffsideAsyncFn: bindIterableTransform @ 'offside async function', 'async function outer_fn() ::',
+      @{} pre_tokens: @[] 'name', 'function', 'name', '(', ')', '{'
+        , post_tokens: @[] '}'
+  , inAsyncArrowFn: bindIterableTransform @ 'vanilla arrow function', 'const outer_arrow = async () => {', '}',
+      @{} pre_tokens: @[] 'const', 'name', '=', 'name', '(', ')', '=>', '{'
+        , post_tokens: @[] '}'
+  , inOffsideAsyncArrowFn: bindIterableTransform @ 'offside arrow function', 'const outer_arrow = async () => ::',
+      @{} pre_tokens: @[] 'const', 'name', '=', 'name', '(', ')', '=>', '{'
+        , post_tokens: @[] '}'
+
+
+const standardTransforms = Object.assign @ {},
+  blockTransforms, functionTransforms, asyncFunctionTransforms
 
 Object.assign @ exports,
   @{} babel_opt
     , genSyntaxTestCases
     , bindIterableTransform
     , standardTransforms
+    , blockTransforms
+    , functionTransforms
+    , asyncFunctionTransforms
     , testSourceTransform
     , testSyntaxError
 
---
+++ test/testForAwaitOfStatements.js
@@ -0,0 +1,168 @@
+require('source-map-support').install()
+const {genSyntaxTestCases, asyncFunctionTransforms} = require('./_xform_syntax_variations')
+
+const tap = require('tap-lite-tester')
+tap.start()
+
+if 0 ::
+  genSyntaxTestCases @ tap,
+    asyncFunctionTransforms.inAsyncFunction @ iterSyntaxVariations()
+else ::
+  for let xform of Object.values @ asyncFunctionTransforms ::
+    genSyntaxTestCases @ tap, xform @ iterSyntaxVariations()
+
+tap.finish()
+
+
+function * iterSyntaxVariations() ::
+  yield * iterForAwaitOfSyntaxErrors()
+  yield * iterForAwaitOfStatements()
+  yield * iterKeywordOffsideForAwaitOfStatements()
+  yield * iterKeywordOffsideForAwaitOfWithCallStatements()
+  yield * iterKeywordAtOffsideForAwaitOfStatements()
+
+
+function * iterForAwaitOfSyntaxErrors() ::
+  yield :: expectSyntaxError: true
+    , title: 'linted for await of statement with expression'
+    , source: @[] 'for await (each of iterable) singleStatement'
+
+  yield :: expectSyntaxError: true
+    , title: 'linted for await of let statement with expression'
+    , source: @[] 'for await (let each of iterable) singleStatement'
+
+  yield :: expectSyntaxError: true
+    , title: 'linted for await of const statement with expression'
+    , source: @[] 'for await (const each of iterable) singleStatement'
+
+
+function * iterForAwaitOfStatements() ::
+  // for await (each of iterable) body variations
+  yield :: expectValid: true
+    , title: 'vanilla for await of statement'
+    , source: @[] 'for await (each of iterable) { blockStatement }'
+    , tokens: @[] 'for', 'name', '(', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'vanilla for await of let statement'
+    , source: @[] 'for await (let each of iterable) { blockStatement }'
+    , tokens: @[] 'for', 'name', '(', 'let', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'vanilla for await of const statement'
+    , source: @[] 'for await (const each of iterable) { blockStatement }'
+    , tokens: @[] 'for', 'name', '(', 'const', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'offside for await of statement'
+    , source: @[] 'for await (each of iterable) :: blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'offside for await of statement, multiline'
+    , source: @[] 'for await (each of iterable) ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'offside for await of let statement, multiline'
+    , source: @[] 'for await (let each of iterable) ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'let', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+
+
+function * iterKeywordOffsideForAwaitOfStatements() ::
+  yield :: expectValid: true
+    , title: 'keyword offside for await of statement'
+    , source: @[] 'for await each of iterable :: blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'keyword offside let for await of statement'
+    , source: @[] 'for await let each of iterable :: blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'let', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'keyword offside for await of statement, multiline'
+    , source: @[] 'for await each of iterable ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'ordered unpack keyword offside for await of statement, multiline'
+    , source: @[] 'for await [a,b] of iterable ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '[', 'name', ',', 'name', ']', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'named unpack keyword offside for await of statement, multiline'
+    , source: @[] 'for await {a,b} of iterable ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '{', 'name', ',', 'name', '}', 'name', 'name', ')', '{', 'name', '}'
+
+
+function * iterKeywordOffsideForAwaitOfWithCallStatements() ::
+  yield :: expectValid: true
+    , title: 'keyword offside for await of statement with call'
+    , source: @[] 'for await each of fn_call @ x, y ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'name', 'name', 'name', '(', 'name', ',', 'name', ')', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'keyword offside for await of let statement with call'
+    , source: @[] 'for await let each of fn_call @ x, y ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'let', 'name', 'name', 'name', '(', 'name', ',', 'name', ')', ')', '{', 'name', '}'
+
+
+  yield :: expectValid: true
+    , title: 'ordered unpack keyword offside for await of statement with call'
+    , source: @[] 'for await [a,b] of fn_call @ x, y ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '[', 'name', ',', 'name', ']', 'name', 'name', '(', 'name', ',', 'name', ')', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'named unpack keyword offside for await of statement with call'
+    , source: @[] 'for await {a,b} of fn_call @ x, y ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '{', 'name', ',', 'name', '}', 'name', 'name', '(', 'name', ',', 'name', ')', ')', '{', 'name', '}'
+
+
+function * iterKeywordAtOffsideForAwaitOfStatements() ::
+  yield :: expectValid: true
+    , title: 'keyword @ offside for await of let statement, multiline'
+    , source: @[] 'for await @ let each of iterable ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'let', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'keyword @ offside for await of statement, multiline'
+    , source: @[] 'for await @ each of iterable ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', 'name', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'ordered unpack keyword @ offside for await of statement, multiline'
+    , source: @[] 'for await @ [a,b] of iterable ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '[', 'name', ',', 'name', ']', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'named unpack keyword @ offside for await of statement, multiline'
+    , source: @[] 'for await @ {a,b} of iterable ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '{', 'name', ',', 'name', '}', 'name', 'name', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'ordered unpack keyword @ offside for await of statement with call'
+    , source: @[] 'for await @ [a,b] of fn_call @ x, y ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '[', 'name', ',', 'name', ']', 'name', 'name', '(', 'name', ',', 'name', ')', ')', '{', 'name', '}'
+
+  yield :: expectValid: true
+    , title: 'named unpack keyword @ offside for await of statement with call'
+    , source: @[] 'for await @ {a,b} of fn_call @ x, y ::'
+                , '  blockStatement'
+    , tokens: @[] 'for', 'name', '(', '{', 'name', ',', 'name', '}', 'name', 'name', '(', 'name', ',', 'name', ')', ')', '{', 'name', '}'
+
